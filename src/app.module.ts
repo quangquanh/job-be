d54ca5616bf99a764e8023660825edf2d001d0c3
Thanhdo1212 <thanhdo12122001@gmail.com>
2024-09-17 17:05:04 +0700

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersNoSpec\Module } from './users--no-spec/users--no-spec/.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    // MongooseModule.forRoot(
+    //   'mongodb+srv://thanhdo12122001:Thanhdo1212%40@cluster0.y1lmn.mongodb.net/',
+    // ),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UsersNoSpec\Module, UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
